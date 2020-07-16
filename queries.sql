8d9ea17d5664f5e6767dc2e6d7cb40ac18a6ae78
mrvnre <mrvnre24@gmail.com>
2020-07-16 11:41:46 -0500

    completed SQL tasks

--- queries.sql
+++ queries.sql
@@ -1,10 +1,22 @@
 ## Part 1: Test it with SQL
+
 SHOW columns
 FROM techjobs.job;
+
 ## Part 2: Test it with SQL
+
 SELECT name
 FROM techjobs.employer
 WHERE location = "St. Louis City";
+
 ## Part 3: Test it with SQL
+
 DROP TABLE techjobs.job;
+
 ## Part 4: Test it with SQL
+
+ SELECT name, description
+ FROM skill
+ LEFT JOIN job_skills ON job_skills.skills_id = skill.id
+ WHERE skills_id IS NOT NULL
+ ORDER BY name;
